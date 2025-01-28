0b4a686e3bf8cf09ba1d93626eaf1767a41e6370
Mohammad Danish Sheikh <120654416+Danish811@users.noreply.github.com>
2025-01-28 14:22:47 +0530

    Time: 3 ms (84.06%) | Memory: 94 MB (85.64%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
     int n,m;
-    vector<vector<int>> Visi;
-    int BFS(vector<vector<int>> &grid, int i, int j){
+    int DFS(vector<vector<int>> &grid, int i, int j){
         if(i<0 || j<0 || i>=n || j>=m) return 0;
         if(grid[i][j] == 0) return 0;
-        if(Visi[i][j]) return 0;
-        Visi[i][j] = true;
+       
         int cnt = grid[i][j];
-        cnt += BFS(grid,i-1,j);
-        cnt += BFS(grid,i+1,j);
-        cnt += BFS(grid,i,j+1);
-        cnt += BFS(grid,i,j-1);
+        grid[i][j] = 0;
+        cnt += DFS(grid,i-1,j);
+        cnt += DFS(grid,i+1,j);
+        cnt += DFS(grid,i,j+1);
+        cnt += DFS(grid,i,j-1);
         return cnt;
 
     }
     int findMaxFish(vector<vector<int>>& grid) {
         n = grid.size();
         m = grid[0].size();
-        Visi.resize(n);
-        for(auto &v: Visi){
-            v.resize(m);
-        }
+      
         int maxi = 0;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(!Visi[i][j] && grid[i][j]!=0){
-                    maxi = max(maxi, BFS(grid,i,j));
+                if( grid[i][j]!=0){
+                    maxi = max(maxi, DFS(grid,i,j));
                 }
             }
         }
