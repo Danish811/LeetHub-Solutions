5c1001163a8f4f85e55e4013dacca874dbf0ffc2
Mohammad Danish Sheikh <120654416+Danish811@users.noreply.github.com>
2025-03-09 14:38:32 +0530

    Time: 3 ms (45.48%) | Memory: 12.8 MB (30.08%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -2,11 +2,17 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         vector<int> res(nums1.size(),-1);
+        vector<int> Suff(nums2.size(), nums2.back());
+        for(int i=nums2.size()-2; i>=0; i--){
+            Suff[i] = min(Suff[i+1],nums2[i]);
+        }
+        map<int,int> M;
+        for(int i=0; i < nums2.size(); i++){
+            M[nums2[i]] = i;
+        }
         for(int i=0; i<nums1.size(); i++){
-            int j=0;
-            while(j<nums2.size() && nums1[i]!=nums2[j]) j++;
-            
-            while(j<nums2.size() && nums2[j] <= nums1[i]) j++;
+            int j = M[nums1[i]];
+            while(j<nums2.size() && nums2[j] <= nums1[i]){ j++; }
             if(j<nums2.size()){
                 res[i] = nums2[j];
             }
